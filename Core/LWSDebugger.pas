e8c2e1eb0f945d8a656fe74d2e11ce86d4dc4785
Silvio Clecio <silvioprog@gmail.com>
2012-03-06 08:35:36 -0300

    ----

--- Core/LWSDebugger.pas
+++ Core/LWSDebugger.pas
@@ -25,28 +25,28 @@ type
   ELWSDebugger = class(Exception);
 
 var
-  LWS_DEBUG_DIR: string = '';
-  LWS_DEBUG_FILENAME: TFileName = '';
-  LWS_DEBUGHEAP_FILENAME: TFileName = '';
+  LWS_DEBUG_DIR: string = ES;
+  LWS_DEBUG_FILENAME: TFileName = ES;
+  LWS_DEBUGHEAP_FILENAME: TFileName = ES;
 
 { Init debugger. }
-procedure LWSInitDebugger(const ADebugDir: string = '');
+procedure LWSInitDebugger(const ADebugDir: string = ES);
 { Send msg to debug file. }
 procedure LWSSendMsg(const AMsg: string);
 { Send stream to debug file. }
 procedure LWSSendStream(AStream: TStream);
 { Send method enter to debug file. }
 procedure LWSSendMethodEnter(const AMethodName: string;
-  const ALineBreak: ShortString = '');
+  const ALineBreak: ShortString = ES);
 { Send method exit to debug file. }
 procedure LWSSendMethodExit(const AMethodName: string;
-  const ALineBreak: ShortString = '');
+  const ALineBreak: ShortString = ES);
 { Send begin msg to file. }
 procedure LWSSendBegin(const AProjectName, AWelcomeMsg: string;
-  const ALineBreak: ShortString = '');
+  const ALineBreak: ShortString = ES);
 { Send end msg to file. }
 procedure LWSSendEnd(const AProjectName, AGoodByeMsg: string;
-  const ALineBreak: ShortString = '');
+  const ALineBreak: ShortString = ES);
 
 implementation
 
@@ -63,7 +63,7 @@ end;
 procedure LWSInitDebugger(const ADebugDir: string);
 begin
   LWS_DEBUG_DIR := ADebugDir;
-  if LWS_DEBUG_DIR = '' then
+  if LWS_DEBUG_DIR = ES then
     LWS_DEBUG_DIR := GetTempDir;
   LWS_DEBUG_DIR := IncludeTrailingPathDelimiter(LWS_DEBUG_DIR);
   if not DirectoryExists(LWS_DEBUG_DIR) then
@@ -71,10 +71,10 @@ begin
       LWS_HTTP_CONTENT_TYPE_TEXT_PLAIN + CRLF + CRLF +
       Format(LWS_DEBUG_SAVE_PATH_NOT_FOUND_ERR,
       [LWS_DEBUG_DIR, 'LWSDebugger unit']));
-  if LWS_DEBUG_FILENAME = '' then
+  if LWS_DEBUG_FILENAME = ES then
     LWS_DEBUG_FILENAME := LWS_DEBUG_DIR + 'DEBUG.LOG';
 {$IFDEF DEBUGHEAP}
-  if LWS_DEBUGHEAP_FILENAME = '' then
+  if LWS_DEBUGHEAP_FILENAME = ES then
   begin
     LWS_DEBUGHEAP_FILENAME := LWS_DEBUG_DIR + 'DEBUGHEAP.LOG';
     DeleteFile(LWS_DEBUGHEAP_FILENAME);
